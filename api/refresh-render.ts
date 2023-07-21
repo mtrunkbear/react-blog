cd2b4ad0c091f8e01eeacde32b61576c3a769aef
mtrunkbear <migueltroncosogaldames@gmail.com>
2023-07-21 17:43:30 -0400

    HOT-FIX edge function

--- api/refresh-render.ts
+++ api/refresh-render.ts
@@ -1,7 +1,18 @@
+import type { RequestContext } from "@vercel/edge";
+
 export const config = {
   runtime: "edge", // this is a pre-requisite
 };
+
 const API_URL = process.env["VITE_API_URL"];
-export default () => {
-  fetch(API_URL).then((response) => console.log(response));
+
+async function refresh() {
+  const res = await fetch("API_URL");
+  return res.ok;
+}
+
+export default (request: Request, context: RequestContext) => {
+  context.waitUntil(refresh().then((ok) => console.log({ ok })));
+
+  return new Response(`Hello, from ${request.url} I'm now an Edge Function!`);
 };
