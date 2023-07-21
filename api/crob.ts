29ba97770ae34388ca497dab74bf07ac7a25dd20
mtrunkbear <migueltroncosogaldames@gmail.com>
2023-07-21 18:09:05 -0400

    HOTFIX  cronjob

--- api/crob.ts
+++ api/crob.ts
@@ -1,3 +1,7 @@
-export default function handler(req, res) {
-    res.status(200).end('Hello Cron!');
-  }
+const API_URL = process.env["VITE_API_URL"];
+
+export default async function handler(req, res) {
+  const result = await fetch(API_URL);
+
+  res.status(200).end("Hello Cron!" + result.status + result.ok);
+}
