6018b4a3694fa91ac1eaa68f03e63cecd585f117
mtrunkbear <migueltroncosogaldames@gmail.com>
2023-08-19 18:49:44 -0400

    HOT-FIX settings/EditForm changed regex of url to URL type validation due bad performance

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,5 +1,5 @@
 export const simpleStringRegex = /^[a-zA-Z0-9_ ]{3,20}$/;
-export function isValidURL(url) {
+export function isValidURL(url:URL) {
     try {
       new URL(url);
       return true;
