28e10aa3eeba454c2d78eef98bc460fe4a197c7b
mtrunkbear <migueltroncosogaldames@gmail.com>
2023-08-19 19:15:02 -0400

    HOT-FIX edit from validation info

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,4 +1,4 @@
-export const simpleStringRegex = /^[a-zA-Z0-9_ ]{3,20}$/;
+export const simpleStringRegex = /^[a-zA-Z0-9_ ]{2,20}$/;
 export function isValidURL(url:URL) {
     try {
       new URL(url);
